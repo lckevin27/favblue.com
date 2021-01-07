b53cd50ad9bf56d4221403aa062cede1be1f8522
lckevin27 <kevin27.cl@gmail.com>
2021-01-07 12:59:10 +0800

    Add a sidebar.

--- client/src/components/TopNav.vue
+++ client/src/components/TopNav.vue
@@ -1,6 +1,14 @@
 <template>
     <nav class="top-nav" v-if="!hideLinks">
       <div v-if="lang === 'en'">
+        <ul class="side-nav">
+          <a href="javascript:void(0)" class="close-btn" v-on:click="closeNav()">&times;</a>
+          <li><router-link to="/" v-on:click="closeNav()">Home</router-link></li>
+          <li><router-link to="/cn/ife" v-on:click="closeNav()">Insightful Front-End</router-link></li>
+          <li><router-link to="/about" v-on:click="closeNav()">About</router-link></li>
+          <li><a href="javascript:void(0)" v-on:click="gotoFooter">Contacts</a></li>
+          <li class="lang-switch"><router-link to="/tocn" v-on:click="closeNav()">中文</router-link></li>
+        </ul>
         <div class="logo-main logo">
             <router-link to="/">favblue<!--img src="../../public/img/logo-main.png" alt=""--></router-link>
         </div>
@@ -19,6 +27,14 @@
           </ul>
       </div>
       <div v-else class="top-nav-cn">
+        <ul class="side-nav">
+          <a href="javascript:void(0)" class="close-btn" v-on:click="closeNav()">&times;</a>
+            <li><router-link to="/cn" v-on:click="closeNav()">首页</router-link></li>
+            <li><router-link to="/cn/ife" v-on:click="closeNav()">洞察前端</router-link></li>
+            <li><router-link to="/cn/about" v-on:click="closeNav()">关于</router-link></li>
+            <li><a href="javascript:void(0)" v-on:click="gotoFooter">联系我</a></li>
+            <li class="lang-switch"><router-link to="/toen" v-on:click="closeNav()">EN</router-link></li>
+        </ul>
         <div class="logo-main logo">
             <router-link to="/cn">favblue</router-link>
         </div>
@@ -53,8 +69,16 @@ export default {
   },
   methods: {
     gotoFooter () {
+      document.getElementsByClassName('side-nav')[0].style.width = '0px'
       const footer = document.getElementById('footer-logo')
       footer.scrollIntoView()
+    },
+    openNav () {
+      document.getElementsByClassName('side-nav')[0].style.width = '400px'
+      document.getElementsByClassName('side-nav')[0].style.maxWidth = '65%'
+    },
+    closeNav () {
+      document.getElementsByClassName('side-nav')[0].style.width = '0px'
     }
   }
 }
